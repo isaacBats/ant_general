b9e47aa5233886cbf5ff3288c6e3b470d680948c
Isaac Batista <disa.indus@gmail.com>
2023-09-12 21:35:44 -0600

    se eliminan mas cosas de wp

--- jgc-content-for-registered-users/uninstall.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Salir si desinstalar/borrar no es llamado por WordPress
-if( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) )
-    exit ();
-
-// Eliminar las opciones del plugin de la base de datos
-delete_option( 'jgccfr_opciones' );
